c55c8d2d55686301bb1c4b6cca39d6e13dc9e466
Vlad Ushakov <zedast14@gmail.com>
2025-03-29 22:49:47 +0300

    Версия 0.2

--- WindowToTray/Form1.Designer.cs
+++ WindowToTray/Form1.Designer.cs
@@ -3,6 +3,16 @@
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
+        private Button btnMinimizeSelected;
+        private NotifyIcon trayIcon;
+        private ListBox listBoxWindows;
+        private Button btnRefresh;
+        private Button btnMinimizeApp;
+        private Button btnClose;
+        private Button btnMaximize;
+        private Button btnMinimize;
+        private Panel titleBar;
+        private Label lblTitle;
 
         protected override void Dispose(bool disposing)
         {
@@ -24,86 +34,140 @@
             listBoxWindows = new ListBox();
             btnRefresh = new Button();
             btnMinimizeApp = new Button();
+            btnClose = new Button();
+            btnMaximize = new Button();
+            btnMinimize = new Button();
+            titleBar = new Panel();
+            lblTitle = new Label();
+            titleBar.SuspendLayout();
             SuspendLayout();
-            // 
+
             // btnMinimizeSelected
-            // 
-            btnMinimizeSelected.BackColor = SystemColors.Control;
-            btnMinimizeSelected.Location = new Point(325, 380);
-            btnMinimizeSelected.Margin = new Padding(4, 3, 4, 3);
+            btnMinimizeSelected.Location = new Point(325, 345);
             btnMinimizeSelected.Name = "btnMinimizeSelected";
             btnMinimizeSelected.Size = new Size(120, 31);
             btnMinimizeSelected.TabIndex = 1;
             btnMinimizeSelected.Text = "Свернуть окно";
-            btnMinimizeSelected.UseVisualStyleBackColor = false;
+            btnMinimizeSelected.UseVisualStyleBackColor = true;
             btnMinimizeSelected.Click += btnMinimizeSelected_Click;
-            // 
+
             // trayIcon
-            // 
             trayIcon.Icon = (Icon)resources.GetObject("trayIcon.Icon");
             trayIcon.Text = "WindowToTray";
             trayIcon.Visible = true;
             trayIcon.DoubleClick += trayIcon_DoubleClick;
-            // 
+
             // listBoxWindows
-            // 
             listBoxWindows.FormattingEnabled = true;
             listBoxWindows.ItemHeight = 15;
-            listBoxWindows.Location = new Point(234, 50);
-            listBoxWindows.Margin = new Padding(4, 3, 4, 3);
+            listBoxWindows.Location = new Point(30, 50);
             listBoxWindows.Name = "listBoxWindows";
-            listBoxWindows.Size = new Size(308, 289);
+            listBoxWindows.Size = new Size(740, 289);
             listBoxWindows.TabIndex = 2;
-            // 
+
             // btnRefresh
-            // 
-            btnRefresh.BackColor = SystemColors.Control;
-            btnRefresh.Location = new Point(234, 345);
-            btnRefresh.Margin = new Padding(4, 3, 4, 3);
+            btnRefresh.Location = new Point(30, 345);
             btnRefresh.Name = "btnRefresh";
             btnRefresh.Size = new Size(120, 31);
             btnRefresh.TabIndex = 3;
             btnRefresh.Text = "Обновить";
-            btnRefresh.UseVisualStyleBackColor = false;
+            btnRefresh.UseVisualStyleBackColor = true;
             btnRefresh.Click += btnRefresh_Click;
-            // 
+
             // btnMinimizeApp
-            // 
-            btnMinimizeApp.BackColor = SystemColors.Control;
-            btnMinimizeApp.Location = new Point(424, 345);
-            btnMinimizeApp.Margin = new Padding(4, 3, 4, 3);
+            btnMinimizeApp.Location = new Point(650, 345);
             btnMinimizeApp.Name = "btnMinimizeApp";
             btnMinimizeApp.Size = new Size(120, 31);
             btnMinimizeApp.TabIndex = 4;
             btnMinimizeApp.Text = "В трей";
-            btnMinimizeApp.UseVisualStyleBackColor = false;
+            btnMinimizeApp.UseVisualStyleBackColor = true;
             btnMinimizeApp.Click += btnMinimizeApp_Click;
-            // 
+
+            // btnClose
+            btnClose.BackColor = Color.Transparent;
+            btnClose.Cursor = Cursors.Hand;
+            btnClose.FlatAppearance.BorderSize = 0;
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnClose.ForeColor = Color.White;
+            btnClose.Location = new Point(754, 0);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(46, 32);
+            btnClose.TabIndex = 5;
+            btnClose.Text = "×";
+            btnClose.UseVisualStyleBackColor = false;
+
+            // btnMaximize
+            btnMaximize.BackColor = Color.Transparent;
+            btnMaximize.Cursor = Cursors.Hand;
+            btnMaximize.FlatAppearance.BorderSize = 0;
+            btnMaximize.FlatStyle = FlatStyle.Flat;
+            btnMaximize.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            btnMaximize.ForeColor = Color.White;
+            btnMaximize.Location = new Point(708, 0);
+            btnMaximize.Name = "btnMaximize";
+            btnMaximize.Size = new Size(46, 32);
+            btnMaximize.TabIndex = 6;
+            btnMaximize.Text = "□";
+            btnMaximize.UseVisualStyleBackColor = false;
+
+            // btnMinimize
+            btnMinimize.BackColor = Color.Transparent;
+            btnMinimize.Cursor = Cursors.Hand;
+            btnMinimize.FlatAppearance.BorderSize = 0;
+            btnMinimize.FlatStyle = FlatStyle.Flat;
+            btnMinimize.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnMinimize.ForeColor = Color.White;
+            btnMinimize.Location = new Point(662, 0);
+            btnMinimize.Name = "btnMinimize";
+            btnMinimize.Size = new Size(46, 32);
+            btnMinimize.TabIndex = 7;
+            btnMinimize.Text = "_";
+            btnMinimize.UseVisualStyleBackColor = false;
+
+            // titleBar
+            titleBar.BackColor = Color.FromArgb(47, 49, 54);
+            titleBar.Controls.Add(lblTitle);
+            titleBar.Controls.Add(btnMinimize);
+            titleBar.Controls.Add(btnMaximize);
+            titleBar.Controls.Add(btnClose);
+            titleBar.Cursor = Cursors.SizeAll;
+            titleBar.Dock = DockStyle.Top;
+            titleBar.Location = new Point(0, 0);
+            titleBar.Name = "titleBar";
+            titleBar.Size = new Size(800, 32);
+            titleBar.TabIndex = 0;
+
+            // lblTitle
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblTitle.ForeColor = Color.White;
+            lblTitle.Location = new Point(10, 8);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(100, 19);
+            lblTitle.TabIndex = 8;
+            lblTitle.Text = "WindowToTray";
+
             // Form1
-            // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ScrollBar;
+            BackColor = Color.FromArgb(54, 57, 63);
             ClientSize = new Size(800, 450);
+            Controls.Add(titleBar);
             Controls.Add(btnMinimizeApp);
             Controls.Add(btnRefresh);
             Controls.Add(listBoxWindows);
             Controls.Add(btnMinimizeSelected);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(4, 3, 4, 3);
             Name = "Form1";
             Text = "WindowToTray";
             Load += Form1_Load;
             Resize += Form1_Resize;
+            titleBar.ResumeLayout(false);
+            titleBar.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnMinimizeSelected;
-        private System.Windows.Forms.NotifyIcon trayIcon;
-        private System.Windows.Forms.ListBox listBoxWindows;
-        private System.Windows.Forms.Button btnRefresh;
-        private System.Windows.Forms.Button btnMinimizeApp;
     }
 }
--- WindowToTray/Form1.cs
+++ WindowToTray/Form1.cs
@@ -11,7 +11,6 @@ namespace WindowToTray
 {
     public partial class Form1 : Form
     {
-        // WinAPI импорт
         [DllImport("user32.dll", CharSet = CharSet.Unicode)]
         private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
@@ -48,78 +47,25 @@ namespace WindowToTray
         private readonly List<WindowInfo> windows = new List<WindowInfo>();
         private readonly Dictionary<IntPtr, NotifyIcon> minimizedWindows = new Dictionary<IntPtr, NotifyIcon>();
 
-        private Button? btnClose = null;
-        private Button? btnMaximize = null;
-        private Button? btnMinimize = null;
-
         public Form1()
         {
             InitializeComponent();
-            InitializeCustomTitleBar();
             ApplyDiscordStyle();
+            SetupTitleBar();
         }
 
-        private void InitializeCustomTitleBar()
+        private void SetupTitleBar()
         {
-            // Кнопка закрытия
-            btnClose = new Button
-            {
-                Text = "?",
-                FlatStyle = FlatStyle.Flat,
-                Font = new Font("Segoe UI", 12, FontStyle.Bold),
-                ForeColor = Color.White,
-                BackColor = Color.Transparent,
-                Size = new Size(46, 32),
-                Location = new Point(ClientSize.Width - 46, 0),
-                Cursor = Cursors.Hand
-            };
-            btnClose.FlatAppearance.BorderSize = 0;
-            btnClose.FlatAppearance.MouseOverBackColor = Color.FromArgb(232, 17, 35);
             btnClose.Click += (s, e) => Close();
-            Controls.Add(btnClose);
-
-            // Кнопка развернуть/свернуть
-            btnMaximize = new Button
+            btnMaximize.Click += (s, e) =>
             {
-                Text = "?",
-                FlatStyle = FlatStyle.Flat,
-                Font = new Font("Segoe UI", 9, FontStyle.Bold),
-                ForeColor = Color.White,
-                BackColor = Color.Transparent,
-                Size = new Size(46, 32),
-                Location = new Point(ClientSize.Width - 92, 0),
-                Cursor = Cursors.Hand
+                WindowState = WindowState == FormWindowState.Maximized
+                    ? FormWindowState.Normal
+                    : FormWindowState.Maximized;
+                btnMaximize.Text = WindowState == FormWindowState.Maximized ? "?" : "?";
             };
-            btnMaximize.FlatAppearance.BorderSize = 0;
-            btnMaximize.FlatAppearance.MouseOverBackColor = Color.FromArgb(66, 70, 77);
-            btnMaximize.Click += (s, e) => WindowState = WindowState == FormWindowState.Maximized ? FormWindowState.Normal : FormWindowState.Maximized;
-            Controls.Add(btnMaximize);
-
-            // Кнопка свернуть
-            btnMinimize = new Button
-            {
-                Text = "_",
-                FlatStyle = FlatStyle.Flat,
-                Font = new Font("Segoe UI", 12, FontStyle.Bold),
-                ForeColor = Color.White,
-                BackColor = Color.Transparent,
-                Size = new Size(46, 32),
-                Location = new Point(ClientSize.Width - 138, 0),
-                Cursor = Cursors.Hand
-            };
-            btnMinimize.FlatAppearance.BorderSize = 0;
-            btnMinimize.FlatAppearance.MouseOverBackColor = Color.FromArgb(66, 70, 77);
             btnMinimize.Click += (s, e) => WindowState = FormWindowState.Minimized;
-            Controls.Add(btnMinimize);
 
-            // Панель заголовка для перемещения
-            var titleBar = new Panel
-            {
-                BackColor = Color.FromArgb(47, 49, 54),
-                Height = 32,
-                Dock = DockStyle.Top,
-                Cursor = Cursors.SizeAll
-            };
             titleBar.MouseDown += (s, e) =>
             {
                 if (e.Button == MouseButtons.Left)
@@ -128,43 +74,36 @@ namespace WindowToTray
                     SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
                 }
             };
-            Controls.Add(titleBar);
-            titleBar.BringToFront();
-
-            // Перемещаем кнопки на передний план
-            btnClose.BringToFront();
-            btnMaximize.BringToFront();
-            btnMinimize.BringToFront();
         }
 
         private void ApplyDiscordStyle()
         {
-            // Настройки формы
-            this.BackColor = Color.FromArgb(64, 68, 75);
+            // Форма
+            this.BackColor = Color.FromArgb(54, 57, 63);
             this.ForeColor = Color.White;
-
-            // Закругление формы
             this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 15, 15));
 
-            // Стиль ListBox
-            listBoxWindows.BackColor = Color.FromArgb(54, 57, 63);
+            // ListBox
+            listBoxWindows.BackColor = Color.FromArgb(64, 68, 75);
             listBoxWindows.ForeColor = Color.White;
             listBoxWindows.BorderStyle = BorderStyle.None;
             listBoxWindows.Font = new Font("Segoe UI", 10);
-            listBoxWindows.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0,
-                listBoxWindows.Width, listBoxWindows.Height, 10, 10));
 
-            // Стиль кнопок (кроме кнопок заголовка)
-            foreach (Button btn in Controls.OfType<Button>().Where(b => b != btnClose && b != btnMaximize && b != btnMinimize))
+            // Кнопки управления окном
+            btnClose.FlatAppearance.MouseOverBackColor = Color.FromArgb(232, 17, 35);
+            btnMaximize.FlatAppearance.MouseOverBackColor = Color.FromArgb(66, 70, 77);
+            btnMinimize.FlatAppearance.MouseOverBackColor = Color.FromArgb(66, 70, 77);
+
+            // Основные кнопки
+            var buttons = new[] { btnMinimizeSelected, btnRefresh, btnMinimizeApp };
+            foreach (var btn in buttons)
             {
                 btn.FlatStyle = FlatStyle.Flat;
                 btn.FlatAppearance.BorderSize = 0;
                 btn.BackColor = Color.FromArgb(88, 101, 242);
                 btn.ForeColor = Color.White;
                 btn.Font = new Font("Segoe UI", 9, FontStyle.Bold);
-                btn.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btn.Width, btn.Height, 8, 8));
 
-                // Эффекты при наведении
                 btn.FlatAppearance.MouseOverBackColor = Color.FromArgb(71, 82, 196);
                 btn.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 70, 180);
             }
@@ -230,7 +169,7 @@ namespace WindowToTray
             else
             {
                 MessageBox.Show("Выберите окно из списка!", "Ошибка",
-                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -243,15 +182,14 @@ namespace WindowToTray
         private void trayIcon_DoubleClick(object sender, EventArgs e)
         {
             this.Show();
-            this.WindowState = FormWindowState.Normal;
             trayIcon.Visible = false;
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            if (this.WindowState == FormWindowState.Minimized)
+            if (WindowState == FormWindowState.Minimized)
             {
-                this.Hide();
+                Hide();
                 trayIcon.Visible = true;
             }
         }
